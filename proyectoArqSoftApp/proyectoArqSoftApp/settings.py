5a01114cbb5d229adb20433784eb3dc0f942b29a
chokpik laptop asus <camilod529@gmail.com>
2022-05-03 14:44:42 -0500

    Creada variables de entorno

--- proyectoArqSoftApp/proyectoArqSoftApp/settings.py
+++ proyectoArqSoftApp/proyectoArqSoftApp/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@ar6vz5*@cv%d%+yn83%1$=@-o#d--j^di)7ytfv(1ww!5j_6#'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
